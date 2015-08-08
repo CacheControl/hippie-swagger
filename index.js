776f919091a398232224311103c4c3cd741e84c1
Cache Hamm <cache.hamm@welltok.com>
2015-08-07 22:25:45 -0600

    retool to be a wrapper for hippie w/ peer dependency

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 'use strict';
 
-var skeemas = require('skeemas');
+var hippie = require('hippie'),
+    skeemas = require('skeemas');
 
-function handleParameters(spec, path, parameters) {
+hippie.prototype.params = function(parameters) {
+  var self = this;
+  this.swaggerParams = this.swaggerParams || {};
+  Object.keys(parameters).forEach(function(k) {
+    self.swaggerParams[k] = parameters[k];
+  });
+  return this;
+};
+
+function handleParameters(pathSpec, path, parameters) {
+  parameters = parameters || {};
+
   var requestURI = path;
-  if(!spec.parameters || !spec.parameters.length) {
+  if(!pathSpec.parameters || !pathSpec.parameters.length) {
     return requestURI;
   }
-  spec.parameters.forEach(function(param) {
+  pathSpec.parameters.forEach(function(param) {
     //check for existance of required parameters
-    if(parameters[param.name] === undefined && param.required) {
+    if(!Object.keys(parameters).length || parameters[param.name] === undefined && param.required) {
       throw new Error('Missing required parameter: ' + param.name);
     }
     //validate parameter against json-schema definition
@@ -23,27 +35,33 @@ function handleParameters(spec, path, parameters) {
   return requestURI;
 }
 
-module.exports = function(swaggerDef, path, parameters) {
-  parameters = parameters || [];
-  return function(options, next) {
-    if(!swaggerDef.paths[path]) {
-      throw new Error('Swagger spec does not define path: ' + path);
-    }
-    if(!options.method) {
-      throw new Error('options must specify a request method(get, post, delete, etc)');
-    }
-    if(!swaggerDef.paths[path][options.method]) {
-      throw new Error('Swagger spec does not define method: ' + options.method);
-    }
-    var spec = swaggerDef.paths[path][options.method];
+function middleware(swaggerDef, options, next) {
+  if(!swaggerDef.paths[this._url]) {
+    throw new Error('Swagger spec does not define path: ' + this._url);
+  }
+  if(!options.method) {
+    throw new Error('Options must specify a request method(get, post, delete, etc)');
+  }
+  var method = options.method.toLowerCase();
+  if(!swaggerDef.paths[this._url][method]) {
+    throw new Error('Swagger spec does not define method: "' + method + '".  Available paths: ' + Object.keys(swaggerDef.paths[this._url]).join(","));
+  }
+  var pathSpec = swaggerDef.paths[this._url][method];
 
-    //default to json
-    //if(!spec.consumes || spec.consumes.includes('application/json')) {
-    //  //todo - set request options for this.json();
-    //}
-    var requestURI = handleParameters(spec, path, parameters);
-    //todo validate the response using json-schema
-    options.url = options.url + requestURI;
-    next(options);
-  };
+  //todo validate the response using json-schema
+  //default to json
+  //if(!pathSpec.consumes || pathSpec.consumes.includes('application/json')) {
+  //  //todo - set request options for this.json();
+  //}
+
+  options.url = handleParameters(pathSpec, options.url, this.swaggerParams);
+  next(options);
+}
+
+module.exports = function(app, swaggerDef) {
+  var hip = hippie(app);
+
+  hip.use(middleware.bind(hip, swaggerDef));
+
+  return hip;
 }
