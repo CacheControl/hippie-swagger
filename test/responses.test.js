7c3cc44eb9ee6da66db26e41e30c5552cf674e6e
Cache Hamm <cache.hamm@welltok.com>
2015-08-17 22:05:22 -0600

    add errors if response code not mentioned in swagger file

---
+++ test/responses.test.js
@@ -0,0 +1,10 @@
+describe('responses', function() {
+  it('errors when status code is not specified in the swagger file', function(done) {
+    hippie(app, swaggerSchema)
+    .put('/invalid-foos')
+    .end(function(err) {
+      expect(err.message).to.match(/No mention of statusCode: 200/);
+      done();
+    });
+  });
+});
