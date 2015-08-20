2b5879e45a2ac63111cd6f32d8bc155570ffe9ee
Cache Hamm <cache.hamm@welltok.com>
2015-08-20 17:11:23 -0600

    improve error message for invalid response, swagger format tweaks

--- test/support/swagger.js
+++ test/support/swagger.js
@@ -3,195 +3,183 @@
 var swaggerParser = require('swagger-parser');
 
 module.exports = {
-    "swagger": "2.0",
-    "info": {
-        "version": "1.0.0",
-        "title": "Test App"
-    },
-    "paths": {
-        "/foos": {
-            "get": {
-              "description": "List all foos",
-              "parameters": [
-                {
-                    "name": "limit",
-                    "in": "query",
-                    "description": "resultset limiter for pagination",
-                    "required": false,
-                    "type": "number",
-                },
-                {
-                    "name": "offset",
-                    "in": "query",
-                    "description": "resultset offset for pagination",
-                    "required": false,
-                    "type": "number",
-                },
-                {
-                    "name": "X-Total-Count",
-                    "in": "header",
-                    "description": "header example",
-                    "required": false,
-                    "type": "number",
-                }
-            ],
-              "responses": {
-                "200": {
-                    "description": "Successful response",
-                    "schema": {
-                      "type": "array",
-                      "items": {
-                          "$ref": "/foo"
-                      }
-                    }
-                },
+  "swagger": "2.0",
+  "info": {
+    "version": "1.0.0",
+    "title": "Test App"
+  },
+  "paths": {
+    "/foos": {
+      "get": {
+        "description": "List all foos",
+        "parameters": [{
+          "name": "limit",
+          "in": "query",
+          "description": "resultset limiter for pagination",
+          "required": false,
+          "type": "number",
+        }, {
+          "name": "offset",
+          "in": "query",
+          "description": "resultset offset for pagination",
+          "required": false,
+          "type": "number",
+        }, {
+          "name": "X-Total-Count",
+          "in": "header",
+          "description": "header example",
+          "required": false,
+          "type": "number",
+        }],
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "schema": {
+              "type": "array",
+              "items": {
+                "$ref": "/foo"
               }
-            },
-            "post": {
-                "description": "Create a foo",
-                "parameters": [
-                  {
-                    "in": "body",
-                    "name": "body",
-                    "description": "foo object to be added",
-                    "required": true,
-                    "schema": {
-                        "$ref": "/foo"
-                    }
-                  }
-                ],
-                "responses": {
-                  "201": {
-                    "description": "Successful response",
-                    "schema": {
-                        "$ref": "/foo"
-                    }
-                  }
-                }
             }
-        },
-        "/invalid-foos": {
-            "get": {
-              "description": "Server returns data that does not validate",
-              "responses": {
-                "200": {
-                    "description": "Successful response",
-                    "schema": {
-                      "type": "array",
-                      "items": {
-                          "$ref": "/foo"
-                      }
-                    }
-                }
-              }
-            },
-            "post": {
-              "description": "Server returns data that does not validate",
-              "responses": {
-                "200": {
-                    "description": "Server returns data that does not validate",
-                    "schema": {
-                       "$ref": "/foo"
-                    }
-                },
-              },
-              "parameters": [
-                  {
-                    "in": "body",
-                    "name": "body",
-                    "description": "foo object to be added",
-                    "required": true,
-                    "schema": {
-                        "$ref": "/foo"
-                    }
-                  }
-                ],
-            },
-            "delete": {
-                "description": "Server returns data that does not validate",
-                "responses": {
-                    "200": {
-                        "description": "Deleted. No content",
-                    }
-                }
-            },
-            "put": {
-                "description": "Server returns data that does not validate",
-                "responses": {
-                    "204": {
-                        "description": "Updated.  No content.",
-                    }
-                }
-            }
-        },
-        "/foos/{fooId}": {
-            "get": {
-                "description": "Retrieving a foo",
-                "parameters": [
-                    {
-                        "name": "fooId",
-                        "in": "path",
-                        "description": "foo identifier",
-                        "required": true,
-                        "type": "string",
-                        "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "Successful response",
-                        "schema": {
-                            "$ref": "/foo"
-                        }
-                    }
-                }
-            },
-            "delete": {
-                "description": "Deleting a foo",
-                "parameters": [
-                    {
-                        "name": "fooId",
-                        "in": "path",
-                        "description": "foo identifier",
-                        "required": true,
-                        "type": "string",
-                        "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
-                    }
-                ],
-                "responses": {
-                    "204": {
-                        "description": "Deleted. No content",
-                    }
-                }
-            }
+          },
         }
+      },
+      "post": {
+        "description": "Create a foo",
+        "parameters": [{
+          "in": "body",
+          "name": "body",
+          "description": "foo object to be added",
+          "required": true,
+          "schema": {
+            "$ref": "/foo"
+          }
+        }],
+        "responses": {
+          "201": {
+            "description": "Successful response",
+            "schema": {
+              "$ref": "/foo"
+            }
+          }
+        }
+      }
     },
-    "definitions": {
-        "/foo": {
-            "title": "Foo object definition",
-            "description": "Schema for a foo object.",
-            "type": "object",
-            "required": ["id", "description", "orderNumber"],
-            "properties": {
-                "id": {
-                    "description": "primary identifier",
-                    "$ref": "#/definitions/uuid"
-                },
-                "description": {
-                    "description": "an explanation of the foo",
-                    "type": "string",
-                },
-                "orderNumber": {
-                    "description": "the order index of the foo",
-                    "type": "integer",
-                }
+    "/invalid-foos": {
+      "get": {
+        "description": "Server returns data that does not validate",
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "schema": {
+              "type": "array",
+              "items": {
+                "$ref": "/foo"
+              }
             }
-        },
-        "uuid": {
-            "title": "UUID",
-            "description": "Schema defining structure of a string representation of a uuid.",
-            "type": "string",
-            "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
+          }
         }
+      },
+      "post": {
+        "description": "Server returns data that does not validate",
+        "responses": {
+          "200": {
+            "description": "Server returns data that does not validate",
+            "schema": {
+              "$ref": "/foo"
+            }
+          },
+        },
+        "parameters": [{
+          "in": "body",
+          "name": "body",
+          "description": "foo object to be added",
+          "required": true,
+          "schema": {
+            "$ref": "/foo"
+          }
+        }],
+      },
+      "delete": {
+        "description": "Server returns data that does not validate",
+        "responses": {
+          "200": {
+            "description": "Deleted. No content",
+          }
+        }
+      },
+      "put": {
+        "description": "Server returns data that does not validate",
+        "responses": {
+          "204": {
+            "description": "Updated.  No content.",
+          }
+        }
+      }
+    },
+    "/foos/{fooId}": {
+      "get": {
+        "description": "Retrieving a foo",
+        "parameters": [{
+          "name": "fooId",
+          "in": "path",
+          "description": "foo identifier",
+          "required": true,
+          "type": "string",
+          "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
+        }],
+        "responses": {
+          "200": {
+            "description": "Successful response",
+            "schema": {
+              "$ref": "/foo"
+            }
+          }
+        }
+      },
+      "delete": {
+        "description": "Deleting a foo",
+        "parameters": [{
+          "name": "fooId",
+          "in": "path",
+          "description": "foo identifier",
+          "required": true,
+          "type": "string",
+          "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
+        }],
+        "responses": {
+          "204": {
+            "description": "Deleted. No content",
+          }
+        }
+      }
     }
+  },
+  "definitions": {
+    "/foo": {
+      "title": "Foo object definition",
+      "description": "Schema for a foo object.",
+      "type": "object",
+      "required": ["id", "description", "orderNumber"],
+      "properties": {
+        "id": {
+          "description": "primary identifier",
+          "$ref": "#/definitions/uuid"
+        },
+        "description": {
+          "description": "an explanation of the foo",
+          "type": "string",
+        },
+        "orderNumber": {
+          "description": "the order index of the foo",
+          "type": "integer",
+        }
+      }
+    },
+    "uuid": {
+      "title": "UUID",
+      "description": "Schema defining structure of a string representation of a uuid.",
+      "type": "string",
+      "pattern": "^[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}$"
+    }
+  }
 };
