8b5e8e7869cd8df40347f8ce126a3ef0df3106c3
Cache Hamm <cache.hamm@welltok.com>
2015-08-26 10:51:16 -0600

    add basic swagger schema validation

---
+++ test/swagger-validator.test.js
@@ -0,0 +1,37 @@
+var schema = {
+  swagger: "2.0",
+  info: {
+    version: "1.0.0",
+    title: "Valid Application"
+  },
+  paths: {}
+};
+var objectAssign = require('object-assign');
+
+describe('swagger-validator', function() {
+  it('passes valid schemas', function() {
+    expect(function() {
+      hippie(app, schema)
+    }).to.not.throw();
+  });
+
+  describe('errors', function() {
+    it('if a swagger definition is not provided', function() {
+      expect(function() {
+        hippie(app)
+      }).to.throw(/Swagger schema required/);
+    });
+
+    it('if the swagger definition does not include required properties', function() {
+      Object.keys(schema).forEach(function(key) {
+        var invalidSchema = objectAssign({}, schema);
+        delete invalidSchema[key];
+        var regex = new RegExp('Swagger schema missing required property: "' + key + '"', 'g');
+
+        expect(function() {
+          hippie(app, invalidSchema)
+        }).to.throw(regex);
+      });
+    });
+  });
+});
